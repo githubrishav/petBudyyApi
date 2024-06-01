b77b44ba9056e6620f54b17af512c0db6b330aad
Rishav <iamrishavkr@gmail.com>
2024-06-02 00:14:28 +0530

    added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const createError = require('http-errors');
-
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 require('dotenv').config();
 
 const apiRoute = require('./routes/api.route');
